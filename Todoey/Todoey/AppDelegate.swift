aff253d196d6e5b779fc7c477a0d058c8a1f2fc0
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-04-12 16:53:48 +0300

    add_Todoey

---
+++ Todoey/Todoey/AppDelegate.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // print(Realm.Configuration.defaultConfiguration.fileURL)
+        return true
+    }
+    
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+    
+    
+    
+}
